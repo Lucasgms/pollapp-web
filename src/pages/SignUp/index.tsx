c10857666d44ade27635e4fe281b555b5e174de6
Lucas Gregory <lgregory12@hotmail.com>
2020-06-30 19:04:41 -0300

    feat: sign-up page.
    sign-up form submit method.

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -2,18 +2,72 @@ import React, { useRef, useCallback } from 'react';
 import { FiUser, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
+import { useToast } from '../../hooks/toast';
 
 import { Container, Content } from './styles';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
 
-  const handleSubmit = useCallback(() => {
-    console.log('submit');
-  }, []);
+  const handleSubmit = useCallback(
+    async (data: SignUpFormData) => {
+      addToast({
+        type: 'error',
+        title: 'Register error',
+        description:
+          'An error occurred while trying to register, please try again',
+      });
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Name required'),
+          email: Yup.string()
+            .required('E-mail required')
+            .email('Enter a valid e-mail address'),
+          password: Yup.string().min(6, 'At least 6 characters'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        addToast({
+          type: 'success',
+          title: 'Registered successfully',
+          description: 'You have already been able to log-in on PollApp',
+        });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Register error',
+          description:
+            'An error occurred while trying to register, please try again',
+        });
+      }
+    },
+    [addToast],
+  );
 
   return (
     <Container>
