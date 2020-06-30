2b697fd62a785bf8d44c7aed32d105005cb4bf99
Lucas Gregory <lgregory12@hotmail.com>
2020-06-30 18:54:13 -0300

    feat: sign-up page.
    add toast components and hook.

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import GlobalStyle from './styles/global';
 
 import Routes from './routes';
 
+import AppProvider from './hooks';
+
 const App: React.FC = () => (
   <Router>
     <GlobalStyle />
-    <Routes />
+    <AppProvider>
+      <Routes />
+    </AppProvider>
   </Router>
 );
 
