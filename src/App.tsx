811b0d0846d96f7776ebd1c645813cd5672ccc51
Lucas Gregory <lgregory12@hotmail.com>
2020-06-30 11:04:03 -0300

    feat: sign-up page.
    add global styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 import Routes from './routes';
 
-const App: React.FC = () => <Routes />;
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <Routes />
+  </>
+);
 
 export default App;
