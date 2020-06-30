927b78cd503a1a59814932ad8e917caeef3d6745
Lucas Gregory <lgregory12@hotmail.com>
2020-06-30 11:04:56 -0300

    feat: sign-up page.
    improve router root element.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 
 import Routes from './routes';
 
 const App: React.FC = () => (
-  <>
+  <Router>
     <GlobalStyle />
     <Routes />
-  </>
+  </Router>
 );
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import SignUp from '../pages/SignUp';
 
 const Routes: React.FC = () => (
-  <Router>
-    <Switch>
-      <Route path="/" exact component={SignUp} />
-    </Switch>
-  </Router>
+  <Switch>
+    <Route path="/" exact component={SignUp} />
+  </Switch>
 );
 
 export default Routes;
